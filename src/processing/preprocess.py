dba88676794d6ea0808b77733cc237bbdd10bdf2
Fanion Newsome <fanionnewsome@gmail.com>
2020-06-01 01:45:58 -0400

    fixed issue with header appearing multiple times in cleaned output

--- src/processing/preprocess.py
+++ src/processing/preprocess.py
@@ -1,13 +1,29 @@
+# import warnings filter
+from warnings import simplefilter
+# ignore all future warnings
+simplefilter(action='ignore', category=FutureWarning)
+
 import glob
 from os.path import join
 import pandas as pd
-#import dask.dataframe as dd
 from imblearn.under_sampling import RandomUnderSampler
 from imblearn.over_sampling import RandomOverSampler
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import LabelEncoder
-import pickle
+from sklearn.feature_selection import VarianceThreshold
+from numpy import unique
+from time import time
+
+dataRoot = '../../data/processed/'
+datasetFilename = 'CICIDS2017_MasterData.csv'
+datasetClean = 'CICIDS2017_CleanData.csv'
+
+# display complete contents of a dataframe
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', -1)
 
 # define undersampling dictionary
 # used to establish the number of records
@@ -48,44 +64,105 @@ UpSample = {
     'Heartbleed' :                5000
 }
 
-def readData(dataroot,filename, datasetName):
+def removeColumnsByVariance(X_train):
+    # define the transform
+    transform = VarianceThreshold()
 
-    filepath = join(dataroot,filename)
-    data = pd.read_csv(filepath)
+    # transform the input data
+    X_output = transform.fit_transform(X_train)
+
+    return X_output    
+
+def cleanData():
+
+    t0 = time()    
+    filepath = join(dataRoot,datasetFilename)
+    df = pd.read_csv(filepath,low_memory=False)
+
+    # convert inf to nan
+    pd.set_option('mode.use_inf_as_na', True)
 
     # remove the spaces found in the dataset column names
     #
-    data = data.rename(columns=lambda x: x.strip())
+    df = df.rename(columns=lambda x: x.strip())
 
-    #target = data['Label'].apply(lambda x: 0 if 'BENIGN' in x else 1)
+    df = df.dropna( axis=0, how='any')
+    df.replace('Infinity',0.0, inplace=True)
+    df = df.replace(',,', np.nan, inplace=False)
+    df.replace('NaN',0.0, inplace=True)
+
+    for i in df.columns:
+        df = df[df[i] != "Infinity"]
+    df = df[df[i] != np.nan]
+    df = df[df[i] != ",,"]
+    df[['Destination Port','Flow Bytes/s', 'Flow Packets/s']] = df[['Destination Port','Flow Bytes/s', 'Flow Packets/s']].apply(pd.to_numeric) 
+
+    # check the data types
+    if(not __debug__):
+        print(df.dtypes)
+        print(df.shape)
+
+    df['Label'] = df['Label'].apply(lambda x: 0 if 'BENIGN' in x else 1)
+
+    df = df.dropna()
+    df.fillna(0, inplace=True)
+
+    # summarize the number of unique values in each column
+    unique_counts = df.nunique()
+
+    # record columns to delete
+    to_del = [i for i,v in enumerate(unique_counts) if v == 1]
     
-    print("Label Distribution for {}:".format(datasetName))
-    print(data['Label'].value_counts())
+    if(len(to_del) > 0):
+        # drop useless columns
+        df.drop(df.columns[to_del], axis=1, inplace=True)
+    
+    X = df.iloc[:,:-1]
+    y = df.iloc[:,-1]
+
+    print(X.shape, y.shape)
+
+    X_new = removeColumnsByVariance(X)
+
+    print(X_new.shape)
+
+    #data.to_csv(datasetClean)
+
+    tt = time() - t0
+    print("Data Clean took {} seconds".format(round(tt,3)))
+    return df
+
+    """
+    #print("Label Distribution:")
+    #print(data['Label'].value_counts())
 
     nullColumns = data.columns[data.isnull().any()]
-    #print(data[nullColumns].isnull().sum())
-
-    data=data.dropna( axis=0, how='any')
-    data=data.replace(',,', np.nan, inplace=False)
-    data.replace("Infinity", 0, inplace=True) 
-
-
-
-    #pd.set_option('display.max_rows', data.shape[0])
-
-    # determine 
-    nullValues = data[data.isnull().any(1)]
-    print(nullValues['Label'].value_counts())
 
     if(len(nullColumns)):
         data = data.dropna(axis=1, how='any')
 
+    if __debug__:
+        print("NULL Columns: ")
+        print(data[nullColumns].isnull().sum())
+
+    #data=data.dropna( axis=0, how='any')
+    #data=data.replace(',,', np.nan, inplace=False)
+    #data.replace("Infinity", 0, inplace=True) 
+
+    # determine if there are any NULL values
+    nullValues = data[data.isnull().any(1)]
+    
+    if __debug__:
+        print("NULL Values:")
+        print(nullValues['Label'].value_counts())   
+
     # delete any record having a NULL value in the dataset
     #
+    
     if(len(nullValues) > 0):
-        print("Cleaning NULL values from dataset '{}'".format(datasetName))
+        print("Removing NULL values from dataset")
         data = data.dropna(axis=0, how='any')
-
+    
      # remove any columns that could cause issues
     #dropColumns = ['Flow Packets/s','Flow Bytes/s']
 
@@ -94,7 +171,9 @@ def readData(dataroot,filename, datasetName):
     #data = data.drop(columns = dropColumns)
     #print("Dropping problematic columns: {}".format(dropColumns))
 
-    data.replace('Infinity',0.0, inplace=True)
+    #data.replace('Infinity',0.0, inplace=True)
+    
+    print("Replacing 'NaN' with 0's...")
     data.replace('NaN',0.0, inplace=True)
 
     #tmpdata = data.copy()
@@ -106,18 +185,19 @@ def readData(dataroot,filename, datasetName):
     #print("Dropped the following constant features: {}".format(','.join(constant_features)))
 
     # fill any columns w/ missing features with a mean value
-    data.fillna(data.mean(), inplace = True) 
+    #data.fillna(data.mean(), inplace = True) 
 
     # lets count if there is NaN values in our dataframe( AKA missing features)
     assert data.isnull().sum().sum()==0, "There should not be any NaN"
 
     #print("Completed dropping columns: {}".format(dropColumns))
-    print("Completed cleaning of dataset '{}'".format(datasetName))
-    print("Finished Reading Data File '{}'...".format(filename))
+    print("Completed cleaning dataset")
+    print("Finished Reading Data File: {}...".format(datasetFilename))
 
     print(len(data.columns))
 
     return data
+    """
 
 def scaleData(data):
     
@@ -141,38 +221,21 @@ def balanceData(attacks, y):
     print(attacks.describe())
     print(downSampleAttacksLabel.describe())
 
-    upSampleAttacks, upSampleAttacksLabel = upSample.fit_sample(downSampleAttacks,downSampleAttacksLabel)
-    print(upSampleAttacksLabel.describe())
+    #upSampleAttacks, upSampleAttacksLabel = upSample.fit_sample(downSampleAttacks,downSampleAttacksLabel)
+    #print(upSampleAttacksLabel.describe())
     
-    print(upSampleAttacks.describe())
+    #print(upSampleAttacks.describe())
 
-    return upSampleAttacks, upSampleAttacksLabel
+    #return upSampleAttacks, upSampleAttacksLabel
 
 def load_data(dataroot):
     
     print("Start Loading Data...")
 
-    mondayWorkingHours = 'Monday-WorkingHours.pcap_ISCX.csv'
-    tuesdayWorkingHours = 'Tuesday-WorkingHours.pcap_ISCX.csv'
-    wednesdayWorkingHours = 'Wednesday-workingHours.pcap_ISCX.csv'
-    thursdayMorningHours = 'Thursday-WorkingHours-Afternoon-Infilteration.pcap_ISCX.csv'
-    thursdayAfternoonHours = 'Thursday-WorkingHours-Morning-WebAttacks.pcap_ISCX.csv'
-    fridayMorningFile = 'Friday-WorkingHours-Morning.pcap_ISCX.csv'
-    fridayAfternoonPortScan = 'Friday-WorkingHours-Afternoon-PortScan.pcap_ISCX.csv'
-    fridayAfternoonDDoS = 'Friday-WorkingHours-Afternoon-DDos.pcap_ISCX.csv'
-
-    bot = readData(dataroot, fridayMorningFile, 'Bot')
-    benign = readData(dataroot, mondayWorkingHours, 'Benign')
-    ssh_ftp = readData(dataroot, tuesdayWorkingHours, 'SSH_FTP')
-    dos_hulk = readData(dataroot, wednesdayWorkingHours, 'DoS Hulk')
-    web = readData(dataroot, thursdayMorningHours, 'Web Attack')
-    infiltration = readData(dataroot, thursdayAfternoonHours, 'Infiltration')
-    ddos = readData(dataroot, fridayAfternoonDDoS, 'DDoS')
-    port_scan = readData(dataroot, fridayAfternoonPortScan, 'PortScan')
-
-    data = pd.concat([benign, bot, ddos, dos_hulk, infiltration, port_scan, ssh_ftp, web], ignore_index=True)
-    train_X = data.drop(columns=['Label'])
-    train_y = data[['Label']]
+    data = cleanData()
+    
+    #train_X = data.drop(columns=['Label']).copy()
+    #train_y = data[['Label']].copy()
     #print(train_X.head())
     #print(train_y['Label'].value_counts())
     #labelencoder = LabelEncoder()
@@ -183,7 +246,8 @@ def load_data(dataroot):
     #y = labelencoder.fit_transform(y)
     #y = data.iloc[:,-1]
     #y = labelencoder.fit_transform(y)
-    X_train, y_train = balanceData(train_X,train_y)
+    #X_train, y_train = balanceData(train_X,train_y)
+    #balanceData(train_X,train_y)
     #X = data.iloc[:,0:len(data.columns)-1].abs()
     #y = data.iloc[:,-1]
     
